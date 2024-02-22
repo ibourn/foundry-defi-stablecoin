2d8c9a9aea8748e7bcb417fdd92206761be13fc9
ibourn <igor.bournazel@orange.fr>
2024-02-22 19:36:25 +0100

    :necktie: added : DecentralizedStableCoin contract

---
+++ TODO.md
@@ -0,0 +1,6 @@
+1. Relative stability : Anchored or pegged -> $1.00
+   1. Chainlink price feed
+   2. Set a func to exchange ETH & BTC -> $
+2. Stability mechanism (minting) : Algorithmic (decentralized)
+   1. can only mint the stablecoin with enough collateral
+3. Collateral : Exogenous (cryptos) : ETH, BTC
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity 0.8.20;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ * @title DecentralizedStableCoin
+ * @author ibourn
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+ * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the DSCEngine smart contract.
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    /* errors */
+    error DecentralizedStableCoin_MustBeMoreThanZero();
+    error DecentralizedStableCoin_BurnAmountExceedsBalance();
+    error DecentralizedStableCoin_NotZeroAddress();
+
+    /*
+     * @dev ERC20Burnable is ERC20
+     */
+    constructor()
+        ERC20("Decentralized Stable Coin", "DSC")
+        Ownable(msg.sender)
+    {
+        _mint(msg.sender, 1000000000000000000000000000);
+    }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin_BurnAmountExceedsBalance();
+        }
+        /* call the burn function from the parent contract : super cause overriding 'burn'*/
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) public onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin_NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin_MustBeMoreThanZero();
+        }
+        /* we don't override '_mint', only call it */
+        _mint(_to, _amount);
+        return true;
+    }
+}
